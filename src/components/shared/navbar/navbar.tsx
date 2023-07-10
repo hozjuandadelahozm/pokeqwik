367550d144ee032ccf5d1cf5f8500b9252c57c45
Juan David De la hoz Montero <juan.delahoz@silvercredit.co>
2023-07-10 17:51:21 -0500

    first commit

--- src/components/shared/navbar/navbar.tsx
+++ src/components/shared/navbar/navbar.tsx
@@ -1,8 +1,11 @@
 import { component$ } from "@builder.io/qwik";
-import { QwikLogo } from "../icons/qwik";
-import styles from "./header.module.css";
+import { QwikLogo } from "../../icons/qwik";
+import styles from "./navbar.module.css";
 
 export default component$(() => {
+
+  console.log(styles.header);
+
   return (
     <header class={styles.header}>
       <div class={["container", styles.wrapper]}>
@@ -14,26 +17,10 @@ export default component$(() => {
         <ul>
           <li>
             <a
-              href="https://qwik.builder.io/docs/components/overview/"
-              target="_blank"
-            >
-              Docs
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://qwik.builder.io/examples/introduction/hello-world/"
-              target="_blank"
-            >
-              Examples
-            </a>
-          </li>
-          <li>
-            <a
               href="https://qwik.builder.io/tutorial/welcome/overview/"
               target="_blank"
             >
-              Tutorials
+              Tutoriales
             </a>
           </li>
         </ul>
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,9 +1,7 @@
 import { component$, Slot, useStyles$ } from "@builder.io/qwik";
-import { routeLoader$ } from "@builder.io/qwik-city";
 import type { RequestHandler } from "@builder.io/qwik-city";
 
-import Header from "~/components/starter/header/header";
-import Footer from "~/components/starter/footer/footer";
+import Nabvar from "~/components/shared/navbar/navbar";
 
 import styles from "./styles.css?inline";
 
@@ -18,21 +16,14 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
   });
 };
 
-export const useServerTimeLoader = routeLoader$(() => {
-  return {
-    date: new Date().toISOString(),
-  };
-});
-
 export default component$(() => {
   useStyles$(styles);
   return (
     <>
-      <Header />
-      <main>
+      <Nabvar />
+      <main class="flex flex-col items-center justify-center">
         <Slot />
       </main>
-      <Footer />
     </>
   );
 });
